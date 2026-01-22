f3d2a0d015b664949479db88aa8325e4f694fa40
t2025-m0077 <t2025-m0077@t2025-m0077ui-MacBookAir.local>
2026-01-22 09:48:23 +0900

    feat: 강의 등록 API 컨트롤러 구현

--- src/main/java/com/github/sleeplessspecialist/peaktime/domain/course/controller/CourseController.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/course/controller/CourseController.java
@@ -1,14 +1,44 @@
 package com.github.sleeplessspecialist.peaktime.domain.course.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.sleeplessspecialist.peaktime.domain.course.dto.CourseRegisterReq;
+import com.github.sleeplessspecialist.peaktime.domain.course.dto.CourseRegisterRes;
+import com.github.sleeplessspecialist.peaktime.domain.course.service.CourseService;
+import com.github.sleeplessspecialist.peaktime.global.common.response.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
 /**
- * CourseController 클래스입니다.
+ * 지식공유자(Lecturer) 관련 강의 관리 API를 제공하는 컨트롤러입니다.
  * <p>
- * TODO: 클래스의 역할을 작성하세요.
+ * 강의 등록, 수정, 삭제 및 지식공유자 본인의 강의 목록 조회 기능을 담당합니다.
+ * 모든 요청은 지식공유자 권한(ROLE_LECTURER)이 필요합니다.
  * </p>
  *
  * @author 기섭
  * @version 1.0
  * @since 2026. 1. 22.
  */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/lecturer/courses")
 public class CourseController {
+
+	private final CourseService courseService;
+
+	/**
+	 * 신규 강의를 등록합니다.
+	 *
+	 * @param req 강의 등록 요청 정보 (제목, 설명, 가격 등)
+	 * @return 등록된 강의 ID를 포함한 응답 객체
+	 */
+	@PostMapping
+	public ApiResponse<CourseRegisterRes> registerCourse(@RequestBody CourseRegisterReq req) {
+		CourseRegisterRes response = courseService.registerCourse(req);
+		return ApiResponse.ok(response);
+	}
 }
