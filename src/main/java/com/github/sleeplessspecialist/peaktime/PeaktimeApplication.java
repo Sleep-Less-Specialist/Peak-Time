a1f4ba3380d257b5e42cab261d063da5b3f461d5
sjww0306 <sjww03@gmail.com>
2026-01-22 16:53:22 +0900

    chore: JWT 설정 클래스 인식을 위한 애플리케이션 설정 보완
    - JwtProperties가 정상적으로 로딩되도록 설정 추가

--- src/main/java/com/github/sleeplessspecialist/peaktime/PeaktimeApplication.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/PeaktimeApplication.java
@@ -2,8 +2,10 @@ package com.github.sleeplessspecialist.peaktime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PeaktimeApplication {
 
 	public static void main(String[] args) {
