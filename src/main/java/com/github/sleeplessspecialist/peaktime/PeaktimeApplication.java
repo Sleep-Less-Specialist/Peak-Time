d8984d0a5e6eefe7fdc7690fe8a7ed3cdfbfa9a8
sjww0306 <sjww03@gmail.com>
2026-01-25 23:17:42 +0900

    refactor: JpaAuditingConfig 클래스 파일 추가 구현 및 수정
    - 테스트(WebMvcTest) 환경에서의 컨텍스트 충돌 가능성을 줄이기 위해 클래스를 별도로 구성

--- src/main/java/com/github/sleeplessspecialist/peaktime/PeaktimeApplication.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/PeaktimeApplication.java
@@ -2,10 +2,8 @@ package com.github.sleeplessspecialist.peaktime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class PeaktimeApplication {
