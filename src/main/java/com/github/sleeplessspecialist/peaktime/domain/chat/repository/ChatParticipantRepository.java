aac864383e26cc99485db71bc5094df79837e929
Juwoojae <juwqq1234@naver.com>
2026-01-26 09:44:14 +0900

    feat: ChatParticipantRepository 자바독 추가 - 자바독 누락으로 자바독 추가

--- src/main/java/com/github/sleeplessspecialist/peaktime/domain/chat/repository/ChatParticipantRepository.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/chat/repository/ChatParticipantRepository.java
@@ -7,9 +7,11 @@ import com.github.sleeplessspecialist.peaktime.domain.chat.entity.ChatRoom;
 import com.github.sleeplessspecialist.peaktime.domain.user.entity.User;
 
 /**
+ 채팅방 참여자(ChatParticipant) 엔티티에 대한 데이터 접근을 담당하는 Repository 인터페이스입니다.
  *
  * <p>
- *
+ * 채팅방 참가 여부 검증, 중복 참가 방지, 접근 권한 확인 등의 용도로 사용됩니다.
+ * 주로 다음과 같은 시나리오에서 활용됩니다:
  * </p>
  *
  * @author 주우재
@@ -19,4 +21,6 @@ import com.github.sleeplessspecialist.peaktime.domain.user.entity.User;
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
 
 	boolean existsByChatRoomAndUser(ChatRoom chatRoom, User user);
+
+	boolean existsByChatRoomIdAndUserId(Long roomId, Long userId);
 }
