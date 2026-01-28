a25d88553d134b3c0a6476c890b74ea42fb05821
t2025-m0077 <t2025-m0077@t2025-m0077ui-MacBookAir.local>
2026-01-28 14:09:13 +0900

    feat: SecurityUtil 구현 및 도메인별 에러 코드 정의
    - SecurityUtil: 로그인한 사용자 정보 추출하는 유틸 클래스 구현 및 공통 예외(CustomException) 적용
    - ErrorCode: 인증 관련 GlobalErrorCode 및 유저 도메인 전용 UserErrorCode 추가

---
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/user/exception/UserErrorCode.java
@@ -0,0 +1,32 @@
+package com.github.sleeplessspecialist.peaktime.domain.user.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.github.sleeplessspecialist.peaktime.global.common.error.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * UserErrorCode Enum 클래스입니다.
+ * <p>
+ * 사용자(User) 도메인에서 발생하는 예외 상황에 대한
+ * HTTP 상태 코드와 에러 메시지를 정의합니다.
+ * </p>
+ *
+ * @author 기섭
+ * @version 1.0
+ * @since 2026. 1. 28.
+ */
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+	PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+	ALREADY_DELETED_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 회원입니다.");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
