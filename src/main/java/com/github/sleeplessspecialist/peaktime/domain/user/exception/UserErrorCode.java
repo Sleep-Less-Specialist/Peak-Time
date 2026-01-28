4b1c139dc01b67c74777dcbb0d024d3573346349
t2025-m0077 <t2025-m0077@t2025-m0077ui-MacBookAir.local>
2026-01-28 14:51:32 +0900

    test: UserService 단위 테스트 및 HTTP 요청 파일 추가
    - Unit Test: UserService의 내 정보 조회/수정 로직 검증 (Mocking 활용)
    - HTTP Client: 실제 API 요청 테스트를 위한 .http 파일 작성fix: UserErrorCode 인터페이스 구현 오류 수정

--- src/main/java/com/github/sleeplessspecialist/peaktime/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/user/exception/UserErrorCode.java
@@ -22,11 +22,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-	PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-	ALREADY_DELETED_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 회원입니다.");
+	USER_NOT_FOUND("U001", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+	DUPLICATE_EMAIL("U002", HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+	PASSWORD_NOT_MATCH("U003", HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+	ALREADY_DELETED_USER("U004", HttpStatus.BAD_REQUEST, "이미 탈퇴한 회원입니다.");
 
+	private final String code;
 	private final HttpStatus httpStatus;
 	private final String message;
 }
