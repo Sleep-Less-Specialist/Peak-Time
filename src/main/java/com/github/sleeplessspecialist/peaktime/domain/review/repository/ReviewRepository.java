bdd725f486ecc59e39dc8cc16edf3204e005b5e9
Juwoojae <juwqq1234@naver.com>
2026-02-05 10:03:28 +0900

    feat: 리뷰 상세 조회, 전체 조회, 갱신 삭제 내부 로직 구현 - 각 구현에 대한 요청, 응답 dto 구현 - 구현 간에 발생할수 있는 예외 코드추가 - review 갱신에 사용되는 엔티티 메서드 추가 - 앞전의 구현과 유사한 방향으로 구현

--- src/main/java/com/github/sleeplessspecialist/peaktime/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/review/repository/ReviewRepository.java
@@ -1,13 +1,15 @@
 package com.github.sleeplessspecialist.peaktime.domain.review.repository;
 
+import com.github.sleeplessspecialist.peaktime.domain.review.entity.Review;
+import com.github.sleeplessspecialist.peaktime.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.github.sleeplessspecialist.peaktime.domain.review.entity.Review;
-
 /**
- *
+ * 리뷰(Review) 엔티티에 대한 영속성 처리를 담당하는 리포지토리입니다.
  * <p>
- *
+ * 리뷰는 수강 이력(Enrollment)에 종속 된다.
  * </p>
  *
  * @author 주우재
@@ -15,4 +17,6 @@ import com.github.sleeplessspecialist.peaktime.domain.review.entity.Review;
  * @since 2026.02.03
  */
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByEnrollment_User(User user, Pageable pageable);
 }
