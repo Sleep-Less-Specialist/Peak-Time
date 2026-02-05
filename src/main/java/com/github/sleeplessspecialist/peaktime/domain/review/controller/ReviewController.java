3447ddd50fb50b167b0af2a17df3ec48c2923166
Juwoojae <juwqq1234@naver.com>
2026-02-05 10:00:22 +0900

    feat: 리뷰 상세 조회, 전체 조회, 갱신 삭제 API 컨트롤러 구현 - 앞전 구현과 유사한 구조로 구현

--- src/main/java/com/github/sleeplessspecialist/peaktime/domain/review/controller/ReviewController.java
+++ src/main/java/com/github/sleeplessspecialist/peaktime/domain/review/controller/ReviewController.java
@@ -1,20 +1,16 @@
 package com.github.sleeplessspecialist.peaktime.domain.review.controller;
 
 
-import com.github.sleeplessspecialist.peaktime.domain.review.dto.CreateReviewReq;
-import com.github.sleeplessspecialist.peaktime.domain.review.dto.CreateReviewRes;
+import com.github.sleeplessspecialist.peaktime.domain.review.dto.*;
 import com.github.sleeplessspecialist.peaktime.domain.review.service.ReviewService;
 import com.github.sleeplessspecialist.peaktime.global.common.response.ApiResponse;
 import com.github.sleeplessspecialist.peaktime.global.common.response.SuccessCode;
-import jakarta.validation.constraints.Min;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 리뷰 관련 API를 제공하는 컨트롤러입니다.
@@ -30,7 +26,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-@Validated
 public class ReviewController {
 
 	private final ReviewService reviewService;
@@ -46,5 +41,55 @@ public class ReviewController {
 
         CreateReviewRes response = reviewService.createReview(userId, enrollmentId, request);
         return ApiResponse.of(SuccessCode.CREATED, response);
+    }
+
+    /**
+     * 리뷰 상세 조회
+     */
+    @GetMapping("/reviews/{reviewId}")
+    public ApiResponse<GetReviewDetailRes> getReviewDetail(
+            @AuthenticationPrincipal Long userId,
+            @Min(1) @PathVariable Long reviewId
+    ) {
+        GetReviewDetailRes response = reviewService.getReviewDetail(userId, reviewId);
+        return ApiResponse.of(SuccessCode.OK, response);
+    }
+
+    /**
+     * 리뷰 목록 조회(내 리뷰)
+     */
+    @GetMapping("/reviews")
+    public ApiResponse<GetReviewListRes> getMyReviews(
+            @AuthenticationPrincipal Long userId,
+            @Min(1) @RequestParam(defaultValue = "1") int page,
+            @Min(1) @Max(50) @RequestParam(defaultValue = "10") int size) {
+
+        GetReviewListRes response = reviewService.getMyReviews(userId, page, size);
+        return ApiResponse.of(SuccessCode.OK, response);
+    }
+
+    /**
+     * 리뷰 수정
+     */
+    @PutMapping("/reviews/{reviewId}")
+    public ApiResponse<UpdateReviewRes> updateReview(
+            @AuthenticationPrincipal Long userId,
+            @Min(1) @PathVariable Long reviewId,
+            @Valid @RequestBody UpdateReviewReq request
+    ) {
+        UpdateReviewRes response = reviewService.updateReview(userId, reviewId, request);
+        return ApiResponse.of(SuccessCode.OK, response);
+    }
+
+    /**
+     * 리뷰 삭제
+     */
+    @DeleteMapping("/reviews/{reviewId}")
+    public ApiResponse<Void> deleteReview(
+            @AuthenticationPrincipal Long userId,
+            @Min(1) @PathVariable Long reviewId
+    ) {
+        reviewService.deleteReview(userId, reviewId);
+        return ApiResponse.ok();
     }
 }
